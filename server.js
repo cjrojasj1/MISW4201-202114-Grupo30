ff4913f18ca026e14781ab610f6795a118ac3abb
Clayderman Rojas <clayderman.rojas@gmail.com>
2021-09-09 22:43:40 -0700

    Ajustes para despliegue de frontend en heroku 17

--- server.js
+++ server.js
@@ -9,4 +9,10 @@ app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+'/front-end/tutorial-canciones/dist/tutorial-canciones/index.html'));
 });
 
+app.use(function(req, res, next) {
+   res.header("Access-Control-Allow-Origin", "*");
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+ });
+
 app.listen(process.env.PORT || 5000);
