c945d76cdec9cf330c1b0d4f0882d2fab01c0eda
jhinariv <jhina.riv@gmail.com>
2021-09-12 01:22:01 -0500

    Configuracion CORS

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://evfmprcffrrhge:d642fd46b24a5b5700373cfd34bb3e7a191a242f921f7a764849598c97c64555@ec2-34-196-238-94.compute-1.amazonaws.com:5432/d6botfbgspjcrl'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ykwkvgzcqpzeei:3e252612d30a34c79b87e104b9393cd67d99da45ddef730a163b9bf34663481f@ec2-44-195-201-3.compute-1.amazonaws.com:5432/d2c9ftgqlhc0up'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY']='frase-secreta'
     app.config['PROPAGATE_EXCEPTIONS'] = True
